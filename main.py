f0dd81b81d1459fccb8cf62f8550dfd9ca99c068
Thiago Alves Soares <thiago.soares@entrepay.io>
2025-08-19 11:07:56 -0300

    Update/Bugs fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 import uuid
 import asyncio
@@ -8,36 +8,47 @@ items = []
 
 class Item(BaseModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4)
-    item: str
-    
-    
+    name: str
+
 @app.get("/")
 async def root():
     await asyncio.sleep(0.1)
-    return {"message": "Welcome to the API"}
-
+    return {"message": "Welcome to the FastAPI application!"}
+    
 @app.post("/items")
-async def create_item(item: Item):
+async def create_item(item: Item, ):
     await asyncio.sleep(0.1)
+    if not item.name or not item.name.strip():
+        raise HTTPException(status_code=400, detail="Item name cannot be empty")
+    item.name = item.name.capitalize()
     items.append(item)
-    return item
-
-
-@app.put("/items/{id}")
-async def update_item(id: int, item: str):
+    return {"message": "The following item was created successfully:", "item": item}
+ 
+@app.get("/items")
+async def list_items():
     await asyncio.sleep(0.1)
-    if 0 <= id < len(items):
-        items[id] = item
-        return {"message": "Item updated", "item": item}
-    else:
-        return {"message": "Item not found"}, 404
+    if not items:
+        raise HTTPException(status_code=404, detail="No items were registered yet, Try creating an item first")   
+    return {"message": "List of items:", "item": items}
 
-@app.delete("/items/{id}")
-async def delete_item(id: int):
+@app.put("/items/{item_id}")
+async def update_item(item_id: uuid.UUID, updated_item: Item):
     await asyncio.sleep(0.1)
-    if 0 <= id:
-        deleted_item = items.pop(id)
-        return {"message": "Item deleted", "item": deleted_item}
-    else:
-        return {"message": "Item not found"}, 404
+    if not items:
+        raise HTTPException(status_code=404, detail="No items were registered yet, Try creating an item first") 
+    for item in items:
+        if item.id == item_id:
+            item.name = updated_item.name.capitalize()
+            return {"message": "The following item was updated successfully:", "item": item}
+            
+    raise HTTPException(status_code=404, detail="Item not found")
 
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: uuid.UUID):
+    await asyncio.sleep(0.1)
+    for idx, item in enumerate(items):
+        if item.id == item_id:
+            deleted_item = items.pop(idx)
+            return {"message": "Item deleted successfully:","item": deleted_item}
+    raise HTTPException(status_code=404, detail="Item not found")
+    
